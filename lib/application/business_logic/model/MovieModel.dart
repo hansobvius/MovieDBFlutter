9aebe9bcfb6e9bdc9a22224793b4390da0b687d4
Thiago Caiado <thiago.caiado@hotmail.com>
2020-11-08 14:17:21 -0300

    impl privider notifier on main function

---
+++ lib/application/business_logic/model/MovieModel.dart
@@ -0,0 +1,7 @@
+class MovieModel{
+
+}
+
+class MovieResults{
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'application/App.dart';
+import 'application/business_logic/view_model/MovieViewModel.dart';
 
 void main() {
-  runApp(App());
+  runApp(ChangeNotifierProvider(
+    create: (context) => MovieViewModel(),
+    child: App()
+  ));
 }
 
 
