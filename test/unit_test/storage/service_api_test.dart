8571533a01b52ff36354742bf9d30c3115fe1f7e
Thiago Caiado <thiago.caiado@hotmail.com>
2021-11-28 15:37:58 -0300

    mock unit test and http test

--- test/unit_test/storage/service_api_test.dart
+++ test/unit_test/storage/service_api_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -5,6 +7,8 @@ import 'package:moviedb_flutter/application/business_logic/model/movie/MovieMode
 import 'package:moviedb_flutter/application/business_logic/model/movie/MovieModelResults.dart';
 import 'package:moviedb_flutter/application/service/ServiceApi.dart';
 import 'package:moviedb_flutter/application/storage/tables/movies/table_helper/MoviesResultsTable.dart';
+import 'package:http/http.dart' as http;
+
 
 import 'service_api_test.mocks.dart';
 
@@ -14,23 +18,36 @@ const String BASE_URL = 'https://api.themoviedb.org/3/movie/';
 const String DEFAULT_LANGUAGE = 'en-US';
 const int DEFAULT_PAGE = 1;
 
+class _MyHttpOverrides extends HttpOverrides {}
+
 @GenerateMocks([ServiceApi])
 void main() {
 
-  MockServiceApi? api;
+  MockServiceApi? mockApi;
+  ServiceApi? api;
+  HttpOverrides.global = _MyHttpOverrides();
 
   group('SERVICE API GROUP TEST', () {
 
     setUp(() {
-      api = MockServiceApi();
+      mockApi = MockServiceApi();
+      api = ServiceApi();
     });
 
-    test('MOVIE REQUEST TEST', () async {
-      when(api!.serviceApi('popular')).thenAnswer((movies) async => Future.value(movies as MovieModel));
+    test('MOVIE REQUEST OBJECT TYPE TEST', () async {
+      when(mockApi!.serviceApi('popular')).thenAnswer((movies) async =>
+          Future.value(movies as MovieModel));
+    });
+
+    test('MOVIE REQUEST OBJECT RESPONSE TEST', () async {
+      await api!.serviceApi("popular").then((value) => {
+        expect(value.results.isNotEmpty, true)
+      });
     });
 
     tearDown(() {
       api = null;
+      mockApi = null;
     });
   });
 }
